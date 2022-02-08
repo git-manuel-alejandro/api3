d3bddd069a23aaf769da97739087c2cb4cb00620
Manuel Roa <manuel.roaojeda23@gmail.com>
2022-02-08 16:37:20 -0300

    setting api docs and redocs

--- posts/api/router.py
+++ posts/api/router.py
@@ -1,7 +1,8 @@
 from django.db import router
 from rest_framework.routers import DefaultRouter
-from posts.api.views import PostViewSet
+from posts.api.views import PostModelViewSet
 
 router_post = DefaultRouter()
 
-router_post.register(prefix='posts', basename='posts', viewset=PostViewSet)
+router_post.register(prefix='posts', basename='posts',
+                     viewset=PostModelViewSet)
--- posts/api/views.py
+++ posts/api/views.py
@@ -1,27 +1,37 @@
+from django import http
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from posts.models import Post
 from posts.api.serializers import PostSerializer
+from rest_framework.viewsets import ViewSet, ModelViewSet
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, IsAuthenticatedOrReadOnly
 
-from rest_framework.viewsets import ViewSet
+
+class PostModelViewSet(ModelViewSet):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    # http_method_names = ['get']
 
 
-class PostViewSet(ViewSet):
-    def list(self, request):
-        serializer = PostSerializer(Post.objects.all(), many=True)
-        return Response(status=status.HTTP_200_OK, data=serializer.data)
+# ============================================================================
 
-    def retrieve(self, request, pk: int):
-        post = PostSerializer(Post.objects.get(pk=pk))
-        return Response(status=status.HTTP_200_OK, data=post.data)
+# class PostViewSet(ViewSet):
+#     def list(self, request):
+#         serializer = PostSerializer(Post.objects.all(), many=True)
+#         return Response(status=status.HTTP_200_OK, data=serializer.data)
 
-    def create(self, request):
-        serializer = PostSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(status=status.HTTP_200_OK, data=serializer.data)
+#     def retrieve(self, request, pk: int):
+#         post = PostSerializer(Post.objects.get(pk=pk))
+#         return Response(status=status.HTTP_200_OK, data=post.data)
 
+#     def create(self, request):
+#         serializer = PostSerializer(data=request.data)
+#         serializer.is_valid(raise_exception=True)
+#         serializer.save()
+#         return Response(status=status.HTTP_200_OK, data=serializer.data)
+# ============================================================================
 
 # class PostApiView(APIView):
 #     def get(self, request):
